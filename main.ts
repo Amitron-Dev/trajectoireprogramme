32dfd18e8895d6a42f0e3f2ba627ad0e49af0dfb
Amitron-Dev <martingrellier79@gmail.com>
2025-03-17 21:08:16 +0100

    Modify functions

--- main.ts
+++ main.ts
@@ -1,13 +1,9 @@
-function img () {
-    basic.showIcon(IconNames.Square)
-}
 input.onLogoEvent(TouchButtonEvent.Pressed, function () {
-    img()
     basic.pause(200)
     parcours(1, 1)
 })
 function parcours (vg: number, vd: number) {
-	
+    basic.showIcon(IconNames.Square)
 }
 basic.showIcon(IconNames.Happy)
 basic.pause(1000)
