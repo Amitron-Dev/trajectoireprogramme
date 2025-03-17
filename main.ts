8e571955c57c318267dcde43d1e82a1cbe64f99d
Amitron-Dev <martingrellier79@gmail.com>
2025-03-17 21:01:19 +0100

    Init Fonction parcours

--- main.ts
+++ main.ts
@@ -4,7 +4,11 @@ function img () {
 input.onLogoEvent(TouchButtonEvent.Pressed, function () {
     img()
     basic.pause(200)
+    parcours(1, 1)
 })
+function parcours (vg: number, vd: number) {
+	
+}
 basic.showIcon(IconNames.Happy)
 basic.pause(1000)
 basic.clearScreen()
