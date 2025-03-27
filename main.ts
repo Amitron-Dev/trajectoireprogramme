8580e69b16f9534b9934192c34926874757e5177
Amitron-Dev <martingrellier79@gmail.com>
2025-03-27 18:23:36 +0100

    Final prog (no time set)

--- main.ts
+++ main.ts
@@ -4,18 +4,40 @@ input.onLogoEvent(TouchButtonEvent.Pressed, function () {
 })
 function parcours (vg: number, vd: number) {
     basic.showIcon(IconNames.Square)
+    // Starting
     Kitronik_Move_Motor.motorOn(Kitronik_Move_Motor.Motors.MotorLeft, Kitronik_Move_Motor.MotorDirection.Forward, vg)
     Kitronik_Move_Motor.motorOn(Kitronik_Move_Motor.Motors.MotorRight, Kitronik_Move_Motor.MotorDirection.Forward, vd)
     basic.pause(1500)
     Kitronik_Move_Motor.stop()
+    // Spin start
     Kitronik_Move_Motor.motorOn(Kitronik_Move_Motor.Motors.MotorLeft, Kitronik_Move_Motor.MotorDirection.Forward, vg)
     basic.pause(250)
     Kitronik_Move_Motor.stop()
+    // Move 2
     Kitronik_Move_Motor.motorOn(Kitronik_Move_Motor.Motors.MotorLeft, Kitronik_Move_Motor.MotorDirection.Forward, vg)
     Kitronik_Move_Motor.motorOn(Kitronik_Move_Motor.Motors.MotorRight, Kitronik_Move_Motor.MotorDirection.Forward, vd)
     basic.pause(1000)
     Kitronik_Move_Motor.stop()
+    // Spin 2
+    Kitronik_Move_Motor.motorOn(Kitronik_Move_Motor.Motors.MotorLeft, Kitronik_Move_Motor.MotorDirection.Forward, vg)
+    basic.pause(250)
+    Kitronik_Move_Motor.stop()
+    // Move 3
+    Kitronik_Move_Motor.motorOn(Kitronik_Move_Motor.Motors.MotorLeft, Kitronik_Move_Motor.MotorDirection.Forward, vg)
+    Kitronik_Move_Motor.motorOn(Kitronik_Move_Motor.Motors.MotorRight, Kitronik_Move_Motor.MotorDirection.Forward, vd)
+    basic.pause(1000)
+    Kitronik_Move_Motor.stop()
+    // Spin 3
+    Kitronik_Move_Motor.motorOn(Kitronik_Move_Motor.Motors.MotorLeft, Kitronik_Move_Motor.MotorDirection.Forward, vg)
+    basic.pause(250)
+    Kitronik_Move_Motor.stop()
+    // Move 4 (faire en sorte qu'il s'arrête juste à la sortie)
+    Kitronik_Move_Motor.motorOn(Kitronik_Move_Motor.Motors.MotorLeft, Kitronik_Move_Motor.MotorDirection.Forward, vg)
+    Kitronik_Move_Motor.motorOn(Kitronik_Move_Motor.Motors.MotorRight, Kitronik_Move_Motor.MotorDirection.Forward, vd)
+    basic.showIcon(IconNames.Yes)
+    basic.pause(1000)
+    basic.clearScreen()
 }
-basic.showIcon(IconNames.Happy)
-basic.pause(200)
+basic.showIcon(IconNames.Yes)
+basic.pause(1000)
 basic.clearScreen()
