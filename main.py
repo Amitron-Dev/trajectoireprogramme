609a5cf2a1e1ed8e8dd0d09b05db1870b4e48c02
Amitron-Dev <martingrellier79@gmail.com>
2025-03-27 14:14:17 +0100

    Init program

--- main.py
+++ main.py
@@ -1,11 +1,36 @@
-def img():
-    basic.show_icon(IconNames.SQUARE)
-
 def on_logo_pressed():
-    img()
     basic.pause(200)
+    parcours(71, 70)
 input.on_logo_event(TouchButtonEvent.PRESSED, on_logo_pressed)
 
-def on_forever():
-    pass
-basic.forever(on_forever)
+def parcours(vg: number, vd: number):
+    basic.show_icon(IconNames.SQUARE)
+    Kitronik_Move_Motor.motor_on(Kitronik_Move_Motor.Motors.MOTOR_RIGHT,
+        Kitronik_Move_Motor.MotorDirection.FORWARD,
+        vd)
+    Kitronik_Move_Motor.motor_on(Kitronik_Move_Motor.Motors.MOTOR_LEFT,
+        Kitronik_Move_Motor.MotorDirection.FORWARD,
+        vg)
+    basic.pause(1000)
+    Kitronik_Move_Motor.motor_off(Kitronik_Move_Motor.Motors.MOTOR_RIGHT)
+    Kitronik_Move_Motor.motor_off(Kitronik_Move_Motor.Motors.MOTOR_LEFT)
+    Kitronik_Move_Motor.motor_on(Kitronik_Move_Motor.Motors.MOTOR_RIGHT,
+        Kitronik_Move_Motor.MotorDirection.FORWARD,
+        vd)
+    basic.pause(150)
+    Kitronik_Move_Motor.motor_off(Kitronik_Move_Motor.Motors.MOTOR_RIGHT)
+    Kitronik_Move_Motor.motor_on(Kitronik_Move_Motor.Motors.MOTOR_LEFT,
+        Kitronik_Move_Motor.MotorDirection.FORWARD,
+        vg)
+    Kitronik_Move_Motor.motor_on(Kitronik_Move_Motor.Motors.MOTOR_RIGHT,
+        Kitronik_Move_Motor.MotorDirection.FORWARD,
+        vd)
+    basic.pause(100)
+    Kitronik_Move_Motor.stop()
+    Kitronik_Move_Motor.motor_on(Kitronik_Move_Motor.Motors.MOTOR_LEFT,
+        Kitronik_Move_Motor.MotorDirection.FORWARD,
+        vg)
+    basic.pause(100)
+basic.show_icon(IconNames.HAPPY)
+basic.pause(200)
+basic.clear_screen()
